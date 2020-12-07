dfb900a5c847447e7b129a99dd3e986fe5de5c53
Nathan Batista <contato@ntbatista.com>
2020-12-07 18:46:36 -0300

    Infos Component

---
+++ components/Infos/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+import styles from "./Infos.module.scss";
+
+export default function Header() {
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        <div className={`${styles.box} ${styles.boxText}`}>
+          <h1>lorem ipsum dolor sit</h1>
+          <strong>amet, consectetur adipiscing elit.</strong>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur elit ut aliquam, purus sit
+            amet luctus venenatis, lectus magna fringilla urna, porttitor
+            rhoncus dolor non enim praesent elementum facilisis leo, vel
+            fringila est ullamcorper eget nulla facilisi etiam dignissim diam
+            quis enim lobortis scelerisque fermentum.
+          </p>
+        </div>
+        <div className={styles.box}>
+          <img src="/defensivos.png" alt="Defensivos" />
+          <div className={styles.shadow}></div>
+          <span className={styles.text}>Defensivos</span>
+        </div>
+
+        <div className={styles.box}>
+          <img src="/nutricao.png" alt="Nutrição" />
+          <div className={styles.shadow}></div>
+          <span className={styles.text}>Nutrição</span>
+        </div>
+
+        <div className={styles.box}>
+          <img src="/fertilizantes.png" alt="Fertilizantes" />
+          <div className={styles.shadow}></div>
+          <span className={styles.text}>Fertilizantes</span>
+        </div>
+
+        <div className={styles.box}>
+          <img src="/sementes.png" alt="Sementes" />
+          <div className={styles.shadow}></div>
+          <span className={styles.text}>Sementes</span>
+        </div>
+
+        <div className={styles.box}>
+          <img src="/graos.png" alt="Grãos" />
+          <div className={styles.shadow}></div>
+          <span className={styles.text}>Grãos</span>
+        </div>
+      </div>
+
+      <a href="#" className={styles.button}>
+        Saiba mais <img src="/arrow.svg" alt="Saiba mais" />
+      </a>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import styles from "../styles/Home.module.scss";
 
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Infos from "../components/Infos";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <Infos />
     </>
   );
 }
